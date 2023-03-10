8fa2072094d7cf2d9d02fc4c33ffd3615d75ca96
NabilKamal1998 <93645942+NabilKamal1998@users.noreply.github.com>
2023-03-10 19:51:45 +0800

    Adding the start of the project

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import requests
 import geocoder
 
 api_key = "2ad0cecd82b29f4b2e289b6777b1e49b"
-g = geocoder.ip('me')
 
+g = geocoder.ip('me')
 lat, lon = g.latlng
 
-url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+url = f"http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit=1&appid={api_key}"
+response = requests.get(url)
+data = response.json()
 
+city = data[0]['name']
+country = data[0]['country']
+
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
 response = requests.get(url)
 data = response.json()
 
@@ -16,4 +22,4 @@ humidity = data['main']['humidity']
 wind_speed = data['wind']['speed']
 weather_desc = data['weather'][0]['description']
 
-print(f"The current weather where you are is {weather_desc}, the temperature is {temperature_celsius}°C, the humidity is {humidity}% and the wind speed is {wind_speed} m/s.")
+print(f"You are currently in {city}, {country}. The current weather is {weather_desc}, the temperature is {temperature_celsius}°C, the humidity is {humidity}% and the wind speed is {wind_speed} m/s.")
