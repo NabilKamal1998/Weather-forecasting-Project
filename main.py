18590de5da9b107451a778f83655a802c692431f
NabilKamal1998 <93645942+NabilKamal1998@users.noreply.github.com>
2023-03-10 19:59:16 +0800

    Adding the start of the project

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import requests
 import geocoder
 
-api_key = "2ad0cecd82b29f4b2e289b6777b1e49b"
+API_KEY = "2ad0cecd82b29f4b2e289b6777b1e49b"
 
-g = geocoder.ip('me')
-lat, lon = g.latlng
 
-url = f"http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit=1&appid={api_key}"
-response = requests.get(url)
-data = response.json()
+def get_location():
+    g = geocoder.ip('me')
+    location = g.city
+    return location, g.latlng
 
-city = data[0]['name']
-country = data[0]['country']
 
-url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
-response = requests.get(url)
-data = response.json()
+def get_weather_data(lat, lon):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
+    response = requests.get(url)
+    weather_data = response.json()
+    return weather_data
 
-temperature_celsius = round(data['main']['temp'] - 273.15, 2)
-humidity = data['main']['humidity']
-wind_speed = data['wind']['speed']
-weather_desc = data['weather'][0]['description']
 
-print(f"You are currently in {city}, {country}. The current weather is {weather_desc}, the temperature is {temperature_celsius}°C, the humidity is {humidity}% and the wind speed is {wind_speed} m/s.")
+def format_output(location, weather_data):
+    temperature_celsius = round(weather_data['main']['temp'] - 273.15, 2)
+    humidity = weather_data['main']['humidity']
+    wind_speed = weather_data['wind']['speed']
+    weather_desc = weather_data['weather'][0]['description']
+    output_string = f"You are currently in {location}. The current weather is {weather_desc}, the temperature is {temperature_celsius}°C, the humidity is {humidity}% and the wind speed is {wind_speed} m/s."
+    return output_string
+
+
+def main():
+    location, (lat, lon) = get_location()
+    weather_data = get_weather_data(lat, lon)
+    output_string = format_output(location, weather_data)
+    print(output_string)
+
+
+if __name__ == '__main__':
+    main()
